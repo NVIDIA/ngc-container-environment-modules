b13818dec71f69d5cb35dd9aa6c0d598314ac971
Scott McMillan <smcmillan@nvidia.com>
2020-07-02 16:39:51 -0500

    Add 20.06 DL containers

---
+++ pytorch/20.06-py3.lua
@@ -0,0 +1,85 @@
+-- The MIT License (MIT)
+--
+-- Copyright (c) 2020 NVIDIA Corporation
+--
+-- Permission is hereby granted, free of charge, to any person obtaining a copy
+-- of this software and associated documentation files (the "Software"), to
+-- deal in the Software without restriction, including without limitation the
+-- rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+-- sell copies of the Software, and to permit persons to whom the Software is
+-- furnished to do so, subject to the following conditions:
+--
+-- The above copyright notice and this permission notice shall be included in
+-- all copies or substantial portions of the Software.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+-- FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+-- IN THE SOFTWARE.
+
+help([==[
+
+Description
+===========
+PyTorch is a GPU accelerated tensor computational framework with a Python front
+end. Functionality can be easily extended with common Python libraries such as
+NumPy, SciPy, and Cython. Automatic differentiation is done with a tape-based
+system at both a functional and neural network layer level. This functionality
+brings a high level of flexibility and speed as a deep learning framework and
+provides accelerated NumPy-like functionality.
+
+More information
+================
+ - NGC: https://ngc.nvidia.com/catalog/containers/nvidia:pytorch
+]==])
+
+whatis("Name: PyTorch")
+whatis("Version: 20.06-py3")
+whatis("Description: PyTorch is a GPU accelerated tensor computational framework with a Python front end. Functionality can be easily extended with common Python libraries such as NumPy, SciPy, and Cython. Automatic differentiation is done with a tape-based system at both a functional and neural network layer level. This functionality brings a high level of flexibility and speed as a deep learning framework and provides accelerated NumPy-like functionality.")
+whatis("URL: https://ngc.nvidia.com/catalog/containers/nvidia:pytorch")
+
+if not (isloaded("Singularity")) then
+    load("Singularity")
+end
+
+conflict(myModuleName(), "rapidsai", "tensorflow")
+
+local image = "nvcr.io_nvidia_pytorch:20.06-py3.sif"
+local uri = "docker://nvcr.io/nvidia/pytorch:20.06-py3"
+local programs = {"python", "python3"}
+local entrypoint_args = ""
+
+-- The absolute path to Singularity is needed so it can be invoked on remote
+-- nodes without the corresponding module necessarily being loaded.
+-- Trim off the training newline.
+local singularity = capture("which singularity | head -c -1")
+
+if (os.getenv("NGC_IMAGE_DIR") and mode() == "load") then
+	image = pathJoin(os.getenv("NGC_IMAGE_DIR"), image)
+
+        if not (isFile(image)) then
+		-- The image could not be found in the container directory
+		LmodMessage("file not found: " .. image)
+		LmodMessage("The container image will be pulled upon first use to the Singularity cache")
+		image = uri
+
+		-- Alternatively, this could pull the container image and
+		-- save it in the container directory
+		--subprocess(singularity .. " pull " .. image .. " " .. uri)
+        end
+else
+        -- Look for the image in the Singularity cache, and if not found
+        -- download it when "singularity run" is invoked.
+        image = uri
+end
+
+local container_launch = singularity .. " run --nv " .. image .. " " .. entrypoint_args
+
+-- Programs to setup in the shell
+for i,program in pairs(programs) do
+        set_shell_function(program, container_launch .. " " .. program .. " $@",
+	                            container_launch .. " " .. program .. " $*")
+end
