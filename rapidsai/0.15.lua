40507f9e95d84cf200355a9faa21236cbb713e39
Scott McMillan <smcmillan@nvidia.com>
2021-01-06 11:46:16 -0600

    Add RAPIDS versions 0.15, 0.16, and 0.17

---
+++ rapidsai/0.15.lua
@@ -0,0 +1,83 @@
+-- The MIT License (MIT)
+--
+-- Copyright (c) 2020 NVIDIA Corporation
+--
+-- Permission is hereby granted, free of charge, to any person obtaining a copy
+-- of this software and associated documentation files (the "Software"), to
+-- deal in the Software without restriction, including without limitation the
+-- rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+-- sell copies of the Software, and to permit persons to whom the Software is
+-- furnished to do so, subject to the following conditions:
+--
+-- The above copyright notice and this permission notice shall be included in
+-- all copies or substantial portions of the Software.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+-- FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+-- IN THE SOFTWARE.
+
+help([==[
+
+Description
+===========
+The RAPIDS suite of software libraries gives you the freedom to execute end-to-end data science and analytics pipelines entirely on GPUs. It relies on NVIDIA® CUDA® primitives for low-level compute optimization, but exposes that GPU parallelism and high-bandwidth memory speed through user-friendly Python interfaces.
+
+More information
+================
+ - NGC: https://ngc.nvidia.com/catalog/containers/nvidia:rapidsai:rapidsai
+]==])
+
+whatis("Name: rapidsai")
+whatis("Version: 0.15")
+whatis("Description: The RAPIDS suite of software libraries gives you the freedom to execute end-to-end data science and analytics pipelines entirely on GPUs. It relies on NVIDIA® CUDA® primitives for low-level compute optimization, but exposes that GPU parallelism and high-bandwidth memory speed through user-friendly Python interfaces.")
+whatis("URL: https://ngc.nvidia.com/catalog/containers/nvidia/rapidsai:rapidsai")
+
+if not (isloaded("Singularity")) then
+    load("Singularity")
+end
+
+conflict(myModuleName(), "pytorch", "tensorflow")
+
+local image = "nvcr.io_nvidia_rapidsai_rapidsai:0.15-cuda10.1-runtime-ubuntu18.04.sif"
+local uri = "docker://nvcr.io/nvidia/rapidsai/rapidsai:0.15-cuda10.1-runtime-ubuntu18.04"
+local programs = {"ipython3", "ipython3", "jupyter", "python", "python3",
+                  "python3.6"}
+local entrypoint_args = ""
+
+setenv("TINI_SUBREAPER", "1")
+
+-- The absolute path to Singularity is needed so it can be invoked on remote
+-- nodes without the corresponding module necessarily being loaded.
+-- Trim off the training newline.
+local singularity = capture("which singularity | head -c -1")
+
+if (os.getenv("NGC_IMAGE_DIR") and mode() == "load") then
+	image = pathJoin(os.getenv("NGC_IMAGE_DIR"), image)
+
+	if not (isFile(image)) then
+		-- The image could not be found in the container directory
+		LmodMessage("file not found: " .. image)
+		LmodMessage("The container image will be pulled upon first use to the Singularity cache")
+		image = uri
+
+		-- Alternatively, this could pull the container image and
+		-- save it in the container directory
+		--subprocess(singularity .. " pull " .. image .. " " .. uri)
+	end
+else
+	-- Look for the image in the Singularity cache, and if not found
+	-- download it when "singularity run" is invoked.
+	image = uri
+end
+
+local container_launch = singularity .. " run --nv " .. image .. " " .. entrypoint_args
+
+-- Programs to setup in the shell
+for i,program in pairs(programs) do
+        set_shell_function(program, container_launch .. " " .. program .. " $@",
+	                            container_launch .. " " .. program .. " $*")
+end
