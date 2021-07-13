e91daa9d4af1fb96b41eec7ebb82a87611800f09
Scott McMillan <smcmillan@nvidia.com>
2021-07-13 09:52:08 -0500

    Add latest NGC containers

---
+++ julia/v2.4.2.lua
@@ -0,0 +1,87 @@
+-- The MIT License (MIT)
+--
+-- Copyright (c) 2021 NVIDIA Corporation
+--
+-- Permission is hereby granted, free of charge, to any person obtaining a copy
+-- of this software and associated documentation files (the "Software"), to
+-- deal in the Software without restriction, including without limitation the
+-- rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+-- sell copies of the Software, and to permit persons to whom the Software is
+-- furnished to do so, subject to the following conditions:
+--
+-- The above copyright notice and this permission notice shall be included in
+-- all copies or substantial portions of the Software.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+-- FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+-- IN THE SOFTWARE.
+
+help([==[
+
+Description
+===========
+The Julia programming language is a flexible dynamic language, appropriate
+for scientific and numerical computing, with performance comparable to
+traditional statically-typed languages.
+
+More information
+================
+ - NGC: https://ngc.nvidia.com/catalog/containers/hpc:julia
+]==])
+
+whatis("Name: julia")
+whatis("Version: v2.4.2")
+whatis("Description: The Julia programming language is a flexible dynamic language, appropriate for scientific and numerical computing, with performance comparable to traditional statically-typed languages.")
+whatis("URL: https://ngc.nvidia.com/catalog/containers/hpc:julia")
+
+if not (os.getenv("NGC_SINGULARITY_MODULE") == "none") then
+	local singularity_module = os.getenv("NGC_SINGULARITY_MODULE") or "Singularity"
+	if not (isloaded(singularity_module)) then
+		load(singularity_module)
+	end
+end
+
+conflict(myModuleName())
+
+local image = "nvcr.io_hpc_julia:v2.4.2.sif"
+local uri = "docker://nvcr.io/hpc/julia:v2.4.2"
+local programs = {"julia"}
+local entrypoint_args = ""
+
+-- The absolute path to Singularity is needed so it can be invoked on remote
+-- nodes without the corresponding module necessarily being loaded.
+-- Trim off the training newline.
+local singularity = capture("which singularity | head -c -1")
+
+if (os.getenv("NGC_IMAGE_DIR") and mode() == "load") then
+	image = pathJoin(os.getenv("NGC_IMAGE_DIR"), image)
+
+        if not (isFile(image)) then
+		-- The image could not be found in the container directory
+		LmodMessage("file not found: " .. image)
+		LmodMessage("The container image will be pulled upon first use to the Singularity cache")
+		image = uri
+
+		-- Alternatively, this could pull the container image and
+		-- save it in the container directory
+		--subprocess(singularity .. " pull " .. image .. " " .. uri)
+        end
+else
+        -- Look for the image in the Singularity cache, and if not found
+        -- download it when "singularity run" is invoked.
+        image = uri
+end
+
+-- /data must exist and be writable inside the container
+local container_launch = singularity .. " run --nv -B $HOME/.julia:/data " .. image .. " " .. entrypoint_args
+
+-- Programs to setup in the shell
+-- julia is the container entrypoint
+for i,program in pairs(programs) do
+        set_shell_function(program, container_launch .. " $@",
+	                            container_launch .. " $*")
+end
---
+++ relion/3.1.2.lua
@@ -0,0 +1,90 @@
+-- The MIT License (MIT)
+--
+-- Copyright (c) 2021 NVIDIA Corporation
+--
+-- Permission is hereby granted, free of charge, to any person obtaining a copy
+-- of this software and associated documentation files (the "Software"), to
+-- deal in the Software without restriction, including without limitation the
+-- rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+-- sell copies of the Software, and to permit persons to whom the Software is
+-- furnished to do so, subject to the following conditions:
+--
+-- The above copyright notice and this permission notice shall be included in
+-- all copies or substantial portions of the Software.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+-- FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+-- IN THE SOFTWARE.
+
+help([==[
+
+Description
+===========
+RELION (for REgularized LIkelihood OptimizatioN) implements an empirical
+Bayesian approach for analysis of electron cryo-microscopy (Cryo-EM).
+Specifically it provides methods of refinement of singular or multiple 3D
+reconstructions as well as 2D class averages. RELION is an important tool in
+the study of living cells.
+
+More information
+================
+ - NGC: https://ngc.nvidia.com/catalog/containers/hpc:relion
+]==])
+
+whatis("Name: relion")
+whatis("Version: 3.1.2")
+whatis("Description: RELION (for REgularized LIkelihood OptimizatioN) implements an empirical Bayesian approach for analysis of electron cryo-microscopy (Cryo-EM). Specifically it provides methods of refinement of singular or multiple 3D reconstructions as well as 2D class averages. RELION is an important tool in the study of living cells.")
+whatis("URL: https://ngc.nvidia.com/catalog/containers/hpc:relion")
+
+if not (os.getenv("NGC_SINGULARITY_MODULE") == "none") then
+	local singularity_module = os.getenv("NGC_SINGULARITY_MODULE") or "Singularity"
+	if not (isloaded(singularity_module)) then
+		load(singularity_module)
+	end
+end
+
+conflict(myModuleName(), "openmpi", "chroma", "lammps", "milc", "quantum_espresso", "relion")
+
+local image = "nvcr.io_hpc_relion:3.1.2.sif"
+local uri = "docker://nvcr.io/hpc/relion:3.1.2"
+local programs = {"mpirun", "relion", "relion_refine_mpi"}
+local entrypoint_args = ""
+
+-- The absolute path to Singularity is needed so it can be invoked on remote
+-- nodes without the corresponding module necessarily being loaded.
+-- Trim off the training newline.
+local singularity = capture("which singularity | head -c -1")
+
+if (os.getenv("NGC_IMAGE_DIR") and mode() == "load") then
+	image = pathJoin(os.getenv("NGC_IMAGE_DIR"), image)
+
+	if not (isFile(image)) then
+		-- The image could not be found in the container directory
+		LmodMessage("file not found: " .. image)
+		LmodMessage("The container image will be pulled upon first use to the Singularity cache")
+		image = uri
+
+		-- Alternatively, this could pull the container image and
+		-- save it in the container directory
+		--subprocess(singularity .. " pull " .. image .. " " .. uri)
+	end
+else
+	-- Look for the image in the Singularity cache, and if not found
+	-- download it when "singularity run" is invoked.
+	image = uri
+end
+
+local container_launch = singularity .. " run --nv " .. image .. " " .. entrypoint_args
+
+-- Multinode support
+setenv("OMPI_MCA_orte_launch_agent", container_launch .. " orted")
+
+-- Programs to setup in the shell
+for i,program in pairs(programs) do
+        set_shell_function(program, container_launch .. " " .. program .. " $@",
+	                            container_launch .. " " .. program .. " $*")
+end
